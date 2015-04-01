5562c7a8b5256d5c834dd365aab7d7b19d08d05f
Nam Mai Anh <nammaianh@hotmail.com>
2015-04-01 16:13:49 +0700

    Update Product: Fix the attributes-losing bug when update product

--- backend/repositories/ProductCategoryRepository.php
+++ backend/repositories/ProductCategoryRepository.php
@@ -66,6 +66,18 @@ class ProductCategoryRepository
         return true;
     }
 
+    /**
+     * Save all changes of an existing product. If its category
+     * is changed, execute processes to change its Product-Category
+     * links.
+     *
+     * @param Product $product
+     * @param integer $newCateId
+     * @return bool
+     * @throws Exception
+     * @throws \Exception
+     * @throws \yii\db\Exception
+     */
     public function updateProduct($product, $newCateId)
     {
         // Create a new TRANSACTION
@@ -81,78 +93,87 @@ class ProductCategoryRepository
             $curCate = $product->mainCategory;
             $newCate = Category::findOne($newCateId);
 
+            // Check is product's category is changed
+            $isCategoryChanged = ($product->mainCategory->id != $newCateId);
+
             /*
-             * 1 - If newCate is descendant of curCate
+             * ONLY execute these Product-Category links changes
+             * if the product's main-category is changed
              */
-            if ($newCate->isDescendantOf($curCate->id)) {
-                // 1.1 - Link the product with the new category
-                $newCate->linkToProduct($productId);
+            if ($isCategoryChanged) {
+                /*
+                 * 1 - If newCate is descendant of curCate
+                 */
+                if ($newCate->isDescendantOf($curCate->id)) {
+                    // 1.1 - Link the product with the new category
+                    $newCate->linkToProduct($productId);
 
-                // 1.2 - Set the new category is main category
-                //       (This will also unset the current main one)
-                $newCate->setAsMainCategoryOf($productId);
+                    // 1.2 - Set the new category is main category
+                    //       (This will also unset the current main one)
+                    $newCate->setAsMainCategoryOf($productId);
 
-                // 1.3 - Link the product with the new category's ancestors
-                //       until hit the current category (currently linked
-                //       and is ancestor)
-                $ancestor = $newCate->parent;
+                    // 1.3 - Link the product with the new category's ancestors
+                    //       until hit the current category (currently linked
+                    //       and is ancestor)
+                    $ancestor = $newCate->parent;
 
-                while ($ancestor != null and $ancestor->id != $newCateId) {
-                    $ancestor->linkToProduct($productId);
-                    $ancestor = $ancestor->parent;
+                    while ($ancestor != null and $ancestor->id != $newCateId) {
+                        $ancestor->linkToProduct($productId);
+                        $ancestor = $ancestor->parent;
+                    }
                 }
-            }
-            /*
-             * 2. If newCate is ancestor of curCate
-             */
-            elseif ($curCate->isDescendantOf($newCateId)) {
-                // 2.1 - Unlink the product and the current main category
-                $curCate->unlinkWithProduct($productId);
+                /*
+                 * 2. If newCate is ancestor of curCate
+                 */
+                elseif ($curCate->isDescendantOf($newCateId)) {
+                    // 2.1 - Unlink the product and the current main category
+                    $curCate->unlinkWithProduct($productId);
 
-                // 2.2 - Unlink all ancestors of the current main category
-                //       until hit the new category (this new is an ancestor
-                //       and will be set to be main right after this step)
-                $ancestor = $curCate->parent;
+                    // 2.2 - Unlink all ancestors of the current main category
+                    //       until hit the new category (this new is an ancestor
+                    //       and will be set to be main right after this step)
+                    $ancestor = $curCate->parent;
 
-                while ($ancestor != null and $ancestor->id != $newCateId) {
-                    $ancestor->unlinkWithProduct($productId);
-                    $ancestor = $ancestor->parent;
+                    while ($ancestor != null and $ancestor->id != $newCateId) {
+                        $ancestor->unlinkWithProduct($productId);
+                        $ancestor = $ancestor->parent;
+                    }
+
+                    // 2.3 - Set the new category as main
+                    $newCate->setAsMainCategoryOf($productId);
                 }
+                /*
+                 * 3. newCate and curCate is on different branch
+                 */
+                else {
+                    // 3.1 - Find the common ancestor of the new and the current categories.
+                    //       The common one can be null if the 2 are absolutely on different
+                    //       branches from the very root.
+                    $commonAncestor = Category::getCommonAncestor($newCateId, $curCate->id);
 
-                // 2.3 - Set the new category as main
-                $newCate->setAsMainCategoryOf($productId);
-            }
-            /*
-             * 3. newCate and curCate is on different branch
-             */
-            else {
-                // 3.1 - Find the common ancestor of the new and the current categories.
-                //       The common one can be null if the 2 are absolutely on different
-                //       branches from the very root.
-                $commonAncestor = Category::getCommonAncestor($newCateId, $curCate->id);
+                    // 3.2 - Unlink the current category and its ancestors until hit the common
+                    $curCate->unlinkWithProduct($productId);
 
-                // 3.2 - Unlink the current category and its ancestors until hit the common
-                $curCate->unlinkWithProduct($productId);
-
-                $ancestor = $curCate->parent;
-                while (($commonAncestor == null and $ancestor != null)
+                    $ancestor = $curCate->parent;
+                    while (($commonAncestor == null and $ancestor != null)
                         or ($commonAncestor != null and $ancestor != null and $ancestor->id != $commonAncestor->id)) {
-                    $ancestor->unlinkWithProduct($productId);
-                    $ancestor = $ancestor->parent;
+                        $ancestor->unlinkWithProduct($productId);
+                        $ancestor = $ancestor->parent;
+                    }
+
+                    // 3.3 - Link the new category and its ancestors until hit the common
+                    $newCate->linkToProduct($productId);
+
+                    $ancestor = $newCate->parent;
+                    while (($commonAncestor == null and $ancestor != null)
+                        or ($commonAncestor != null and $ancestor != null and $ancestor->id != $commonAncestor->id)) {
+                        $ancestor->linkToProduct($productId);
+                        $ancestor = $ancestor->parent;
+                    }
+
+                    // 3.4 - Set the new category as main
+                    $newCate->setAsMainCategoryOf($productId);
                 }
-
-                // 3.3 - Link the new category and its ancestors until hit the common
-                $newCate->linkToProduct($productId);
-
-                $ancestor = $newCate->parent;
-                while (($commonAncestor == null and $ancestor != null)
-                    or ($commonAncestor != null and $ancestor != null and $ancestor->id != $commonAncestor->id)) {
-                    $ancestor->linkToProduct($productId);
-                    $ancestor = $ancestor->parent;
-                }
-
-                // 3.4 - Set the new category as main
-                $newCate->setAsMainCategoryOf($productId);
             }
 
             // Works done, commit the transaction
